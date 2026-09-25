f23742720b3e854bf58427318e190bae80bbd7bf
Your Name <you@example.com>
2026-09-25 17:07:35 +0330

    Store SQLite database on persistent disk

--- config.py
+++ config.py
@@ -7,13 +7,15 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key-change-me")
+
     MENUCI_URL = os.getenv(
         "MENUCI_URL",
         "https://menuci.ir"
-    )    
+    )
+
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "sqlite:///app.db"
+        "sqlite:////opt/app-root/src/data/app.db"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
